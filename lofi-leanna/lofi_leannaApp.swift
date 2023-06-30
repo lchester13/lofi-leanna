fda4a62ef282eff005e980e2e736ec1ac8a89a10
lchester13 <leanna.r.chester@gmail.com>
2023-06-30 13:19:23 -0700

    Initial Commit

---
+++ lofi-leanna/lofi_leannaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lofi_leannaApp.swift
+//  lofi-leanna
+//
+//  Created by Leanna Chester on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct lofi_leannaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
